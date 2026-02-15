7ff1eeedc2b0c9d148560adb707ba8887639d53e
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-02-16 05:33:40 +0800

    Add UPDATE and GET by player id with existing record in inventory table

--- src/DSRS.Application/Contracts/IPlayerRepository.cs
+++ src/DSRS.Application/Contracts/IPlayerRepository.cs
@@ -8,5 +8,6 @@ public interface IPlayerRepository
     Task CreateAsync(Player player);
     Task<Player> GetById(Guid Id);
     Task<Player> GetByIdWithDailyPrices(Guid id);
+    Task<Player> GetByIdWithInventories(Guid id);
     Task<bool> NameExistsAsync(string name);
 }
