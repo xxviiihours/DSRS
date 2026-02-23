614fb2cb88e3cdbf34151ccea17ef5c653198427
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-02-23 23:12:07 +0800

    Add Player Query

---
+++ src/DSRS.Application/Contracts/IPlayerQuery.cs
@@ -0,0 +1,12 @@
+﻿using DSRS.Application.Features.Players;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSRS.Application.Contracts;
+
+public interface IPlayerQuery
+{
+    Task<PlayerDto> GetPlayerByIdAsync(Guid playerId);
+    Task<PlayerDto> GetPlayerByName(string name);
+}
---
+++ src/DSRS.Infrastructure/Queries/PlayerQuery.cs
@@ -0,0 +1,33 @@
+﻿using DSRS.Application.Contracts;
+using DSRS.Application.Features.Players;
+using DSRS.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace DSRS.Infrastructure.Queries;
+
+public class PlayerQuery(AppDbContext context) : IPlayerQuery
+{
+    private readonly AppDbContext _context = context;
+
+    public Task<PlayerDto> GetPlayerByIdAsync(Guid playerId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<PlayerDto> GetPlayerByName(string name)
+    {
+        var player = await _context.Players
+            .AsNoTracking()
+            .Where(p => p.Name == name)
+            .Select(p => new PlayerDto
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Balance = p.Balance,
+            })
+            .SingleOrDefaultAsync();
+
+
+        return player!;
+    }
+}
