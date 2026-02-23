a75dff0cc08d81ddc6b136f427a5dbf6529beda3
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-02-23 23:13:26 +0800

    Added Get Player by Name Query Features

---
+++ src/DSRS.Application/Features/Players/Get/GetPlayerByNameCommand.cs
@@ -0,0 +1,9 @@
+﻿using DSRS.SharedKernel.Primitives;
+using Mediator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSRS.Application.Features.Players.Get;
+
+public record GetPlayerByNameCommand(string Name) : ICommand<Result<PlayerDto>> { }
---
+++ src/DSRS.Application/Features/Players/Get/GetPlayerByNameHandler.cs
@@ -0,0 +1,23 @@
+﻿using DSRS.Application.Contracts;
+using DSRS.SharedKernel.Primitives;
+using Mediator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSRS.Application.Features.Players.Get;
+
+public class GetPlayerByNameHandler(IPlayerQuery playerQuery) : ICommandHandler<GetPlayerByNameCommand, Result<PlayerDto>>
+{
+    private readonly IPlayerQuery _playerQuery = playerQuery;
+
+    public async ValueTask<Result<PlayerDto>> Handle(GetPlayerByNameCommand command, CancellationToken cancellationToken)
+    {
+        var player = await _playerQuery.GetPlayerByName(command.Name);
+        if (player == null) 
+            return Result<PlayerDto>.Failure(
+                new Error("Player.Name.NotFound", $"Player with name {command.Name} does not exists."));
+
+        return Result<PlayerDto>.Success(player);
+    }
+}
