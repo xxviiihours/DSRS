6ad7e94f2808debc110fe561210f7ce75597ccf7
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-01 20:32:45 +0800

    Add migration snapshot

---
+++ src/DSRS.Infrastructure/Persistence/Migrations/Sqlite/20260228201234_AddedPercentageColumnInDailyPriceTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DSRS.Infrastructure.Persistence.Migrations.Sqlite
+{
+    /// <inheritdoc />
+    public partial class AddedPercentageColumnInDailyPriceTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Percentage",
+                table: "DailyPrices",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Percentage",
+                table: "DailyPrices");
+        }
+    }
+}
