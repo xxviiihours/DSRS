0c4bbbd57415c61218e7d78184f3585c755232b1
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-07 07:33:18 +0800

    Added DistributionRepository to use for event handlers

---
+++ src/DSRS.Application/Contracts/IDistributionHistory.cs
@@ -0,0 +1,11 @@
+﻿using DSRS.Domain.Aggregates.Distributions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSRS.Application.Contracts;
+
+public interface IDistributionHistory
+{
+    Task CreateAsync(DistributionRecord record);
+}
---
+++ src/DSRS.Infrastructure/Persistence/Repositories/DistributionHistoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using DSRS.Application.Contracts;
+using DSRS.Domain.Aggregates.Distributions;
+using DSRS.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSRS.Infrastructure.Persistence.Repositories;
+
+public class DistributionHistoryRepository(AppDbContext context) : IDistributionHistory
+{
+    private readonly AppDbContext _context = context;
+
+    public async Task CreateAsync(DistributionRecord record)
+    {
+        await _context.DistributionRecords.AddAsync(record);
+
+        await Task.CompletedTask;
+    }
+}
