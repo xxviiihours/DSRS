1cce02a2fe7ca48f2be0b80431eeef045a967dff
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-07 07:33:52 +0800

    Added DomainEventService for publishing events

---
+++ src/DSRS.Application/Contracts/IDomainEventService.cs
@@ -0,0 +1,11 @@
+﻿using DSRS.SharedKernel.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSRS.Application.Contracts;
+
+public interface IDomainEventService
+{
+    Task Publish(DomainEvent domainEvent, CancellationToken cancellationToken);
+}
---
+++ src/DSRS.Infrastructure/Persistence/Services/DomainEventService.cs
@@ -0,0 +1,36 @@
+﻿using DSRS.Application.Contracts;
+using DSRS.Application.Notifications;
+using DSRS.Domain.Events;
+using DSRS.SharedKernel.Abstractions;
+using Mediator;
+using Microsoft.Extensions.Logging;
+
+namespace DSRS.Infrastructure.Persistence.Services;
+
+public class DomainEventService(IMediator mediator, ILogger<DomainEventService> logger) : IDomainEventService
+{
+    private readonly IMediator _mediator = mediator;
+    private readonly ILogger<DomainEventService> _logger = logger;
+
+    public async Task Publish(DomainEvent domainEvent, CancellationToken cancellationToken)
+    {
+        if (domainEvent is null)
+            return;
+
+        _logger.LogInformation("Publishing domain event. Event - {event}", domainEvent.GetType().Name);
+        var notification = CreateNotification(domainEvent);
+        if (notification == null)
+            _logger.LogWarning($"No handler registered for domain event {domainEvent.GetType().Name}");
+
+        await _mediator.Publish(notification!, cancellationToken);
+    }
+
+    private static INotification? CreateNotification(DomainEvent domainEvent)
+    {
+        return domainEvent switch
+        {
+            ItemPurchasedEvent e => new ItemPurchasedNotification(e),
+            _ => null
+        };
+    }
+}
