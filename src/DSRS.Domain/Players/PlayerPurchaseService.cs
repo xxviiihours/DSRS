eee7a7ad7216beef3bebdf7ecdc0f268f996fa32
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-07 01:45:22 +0800

    Add PlayerPurchaseService to implement daily regeneration limit

---
+++ src/DSRS.Domain/Players/PlayerPurchaseService.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace DSRS.Domain.Players;
+
+public sealed class PlayerPurchaseService
+{
+  private const int DailyIncrease = 25;
+
+  public static void GenerateDailyPurchaseLimit(Player player, DateOnly today)
+  {
+    if (player.LastLimitGeneration == today)
+      return;
+
+    int daysPassed = today.DayNumber - player.LastLimitGeneration.DayNumber;
+
+    if (daysPassed <= 0)
+      return;
+
+    int storageToAdd = daysPassed * DailyIncrease;
+
+    player.RegenerateLimit(storageToAdd);
+
+    player.SetLastGeneration(today);
+  }
+}
