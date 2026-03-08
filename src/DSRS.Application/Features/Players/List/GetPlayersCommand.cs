d6d3fbaf016d0cb161d8f6bb8a6a6c85a62289fe
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-08 21:42:24 +0800

    Additional handler for GetPlayersQuery

---
+++ src/DSRS.Application/Features/Players/List/GetPlayersCommand.cs
@@ -0,0 +1,7 @@
+using DSRS.SharedKernel.Primitives;
+using Mediator;
+using System;
+
+namespace DSRS.Application.Features.Players.List;
+
+public record GetPlayersCommand(string Query) : ICommand<Result<List<PlayerDto>>> { }
---
+++ src/DSRS.Application/Features/Players/List/GetPlayersHandler.cs
@@ -0,0 +1,22 @@
+using DSRS.Application.Contracts;
+using DSRS.SharedKernel.Primitives;
+using Mediator;
+using System;
+
+namespace DSRS.Application.Features.Players.List;
+
+public class GetPlayersHandler(IPlayerQuery playerQuery) : ICommandHandler<GetPlayersCommand, Result<List<PlayerDto>>>
+{
+  private readonly IPlayerQuery _playerQuery = playerQuery;
+
+  public async ValueTask<Result<List<PlayerDto>>> Handle(GetPlayersCommand command, CancellationToken cancellationToken)
+  {
+    var players = await _playerQuery.GetPlayers(command.Query);
+    if (players.Count < 0)
+      return Result<List<PlayerDto>>.Failure(
+        new Error("Player.List.Empty", "Empty player list"));
+
+
+    return Result<List<PlayerDto>>.Success(players);
+  }
+}
