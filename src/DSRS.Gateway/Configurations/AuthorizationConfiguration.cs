8672bbdfbdef141ab01947481b69dd05ddf26af3
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-12 03:23:13 +0800

    Added Authorization Configuration

---
+++ src/DSRS.Gateway/Configurations/AuthorizationConfiguration.cs
@@ -0,0 +1,33 @@
+﻿namespace DSRS.Gateway.Configurations;
+
+public static class AuthorizationConfiguration
+{
+    public static IServiceCollection AddAuthorizationConfiguration(this IServiceCollection services, ILogger logger)
+    {
+        logger.LogInformation("Configuring authorization policies");
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy("authenticated", policy =>
+            {
+                policy.RequireAuthenticatedUser();
+            });
+        });
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowedClients",
+                policy =>
+                {
+                    policy
+                        .WithOrigins("http://localhost:5173") // React app URL
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                });
+        });
+
+        return services;
+    }
+
+
+}
