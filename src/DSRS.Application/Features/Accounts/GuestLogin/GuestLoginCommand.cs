794a675aafe52ebb3d2ffbb7490bd404769de47f
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-11 14:58:12 +0800

    Added Guest Login Feature in Application

---
+++ src/DSRS.Application/Features/Accounts/GuestLogin/GuestLoginCommand.cs
@@ -0,0 +1,12 @@
+﻿using DSRS.Domain.Aggregates.Players;
+using DSRS.SharedKernel.Primitives;
+using Mediator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSRS.Application.Features.Accounts.GuestLogin;
+
+public record GuestLoginCommand : ICommand<Result<Player>>
+{
+}
