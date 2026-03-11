18f19a4d3f79410f7b21f9485603bdb7b1c1744a
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-12 03:23:29 +0800

    Cleanup middleware configuration

--- src/DSRS.Gateway/Program.cs
+++ src/DSRS.Gateway/Program.cs
@@ -13,46 +13,23 @@ var startupLogger = loggerFactory.CreateLogger<Program>();
 
 startupLogger.LogInformation("Starting web host");
 
-
 builder.Services.AddServiceConfigurations(startupLogger, builder);
+builder.Services.AddAuthorizationConfiguration(startupLogger);
 
 builder.Services.AddFastEndpoints()
-                .SwaggerDocument(o =>
-                {
-                    o.ShortSchemaNames = false;
-                    o.DocumentSettings = (document) =>
-                    {
-                        document.Title = "Daily Stock Redistribution System API";
-                        document.Description = "API for the DSRS application";
-                        document.Version = "v1";
-                    };
-                });
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowedClients",
-        policy =>
+    .SwaggerDocument(o =>
+    {
+        o.ShortSchemaNames = false;
+        o.DocumentSettings = (document) =>
         {
-            policy
-                .WithOrigins("http://localhost:5173") // React app URL
-                .AllowAnyHeader()
-                .AllowAnyMethod()
-                .AllowCredentials();
-        });
-});
+            document.Title = "Daily Stock Redistribution System API";
+            document.Description = "API for the DSRS application";
+            document.Version = "v1";
+        };
+    });
 
 var app = builder.Build();
 
-app.UseCors("AllowedClients");
-
 await app.UseAppMiddlewareAndSeedDatabase();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
 
 app.Run();
