e2ea1c166964dacd06448a357a46faa54a49002e
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-12 03:22:17 +0800

    Moved GuestLogin feature to Auth Added Auth Initializer to check existing auth cookie else return 401

---
+++ src/DSRS.Application/Features/Authentications/AuthenticateResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSRS.Application.Features.Authentications;
+
+public class AuthenticateResponse
+{
+    public Guid Id { get; set; }
+    public string UserName { get; set; } = string.Empty;
+}
---
+++ src/DSRS.Application/Features/Authentications/InitAuth/InitAuthHandler.cs
@@ -0,0 +1,30 @@
+﻿using DSRS.Application.Contracts;
+using DSRS.Domain.Aggregates.Players;
+using DSRS.SharedKernel.Primitives;
+using Mediator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSRS.Application.Features.Authentications.InitAuth;
+
+public class InitAuthHandler(ICurrentUserService currentUserService, IPlayerQuery playerQuery) : ICommandHandler<InitAuthCommand, Result<AuthenticateResponse>>
+{
+    private readonly IPlayerQuery _playerQuery = playerQuery;
+    private readonly ICurrentUserService _currentUserService = currentUserService;
+
+    public async ValueTask<Result<AuthenticateResponse>> Handle(InitAuthCommand command, CancellationToken cancellationToken)
+    {
+        var player = await _playerQuery.GetPlayerByIdAsync(_currentUserService.Id);
+
+        if (player == null)
+            return Result<AuthenticateResponse>.Failure(new Error("Player.Claim.NotFound.", "Player not found"));
+
+        return Result<AuthenticateResponse>.Success(
+            new AuthenticateResponse
+        {
+            Id = player.Id,
+            UserName = player.Name
+        });
+    }
+}
