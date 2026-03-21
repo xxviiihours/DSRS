3a1e028d7ad96324f335319174eb666fd7c14433
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-21 21:45:14 +0800

    Applied saving of balance snapshot thru DomainEvent

--- src/DSRS.Domain/Events/ItemSoldEvent.cs
+++ src/DSRS.Domain/Events/ItemSoldEvent.cs
@@ -6,10 +6,12 @@ namespace DSRS.Domain.Events;
 public sealed class ItemSoldEvent(Guid dailyPriceId,
     Guid playerId,
     int quantity,
-    decimal totalCost) : DomainEvent
+    decimal totalCost,
+    decimal balance) : DomainEvent
 {
-  public Guid ItemId { get; init; } = dailyPriceId;
-  public Guid PlayerId { get; init; } = playerId;
-  public int Quantity { get; init; } = quantity;
-  public decimal TotalCost { get; init; } = totalCost;
+    public Guid ItemId { get; init; } = dailyPriceId;
+    public Guid PlayerId { get; init; } = playerId;
+    public int Quantity { get; init; } = quantity;
+    public decimal TotalCost { get; init; } = totalCost;
+    public decimal Balance { get; set; } = balance;
 }
