4bb47ba0b21209024c93f6f0f5b43b0cdef50ec5
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-23 22:55:59 +0800

    Added BackgroundJobOptions

---
+++ src/DSRS.Application/Options/BackgroundJobOption.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSRS.Application.Options;
+
+public class BackgroundJobOption
+{
+    public int GenerateInterval { get; set; }
+    public bool Enabled { get; set; }
+}
