de8b6d572aa6c745d46771cbfb7b0af83a45ea67
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-23 23:02:20 +0800

    Added quartz background job and triggers with cron scheduling

--- src/DSRS.Infrastructure/InfrastuctureServiceExtensions.cs
+++ src/DSRS.Infrastructure/InfrastuctureServiceExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using DSRS.Application.Contracts;
+using DSRS.Application.Options;
 using DSRS.Infrastructure.Identity;
 using DSRS.Infrastructure.Identity.Services;
+using DSRS.Infrastructure.Jobs;
 using DSRS.Infrastructure.Persistence;
 using DSRS.Infrastructure.Persistence.Migrations.Sqlite;
 using DSRS.Infrastructure.Persistence.Migrations.SqlServer;
@@ -12,6 +14,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Quartz;
 
 namespace DSRS.Infrastructure;
 
@@ -49,7 +52,7 @@ public static class InfrastuctureServiceExtensions
         services.AddScoped<IItemRepository, ItemRepository>();
         services.AddScoped<IDailyPriceRepository, DailyPriceRepository>();
         services.AddScoped<IInventoryRepository, InventoryRepository>();
-        services.AddScoped<IDistributionHistory, DistributionHistoryRepository>();
+        services.AddScoped<IDistributionHistoryRepository, DistributionHistoryRepository>();
         services.AddScoped<ISocialRepository, SocialRepository>();
         services.AddScoped<IPlayerSnapshotRepository, PlayerSnapshotRepository>();
 
@@ -66,6 +69,41 @@ public static class InfrastuctureServiceExtensions
 
         services.AddScoped<IIdentityService, IdentityService>();
         services.AddScoped<ICurrentUserService, CurrentUserService>();
+        services.AddScoped<IMarketService, MarketService>();
+
+        services.Configure<BackgroundJobOption>(config.GetSection("BackgroundJobs"));
+
+        services.AddQuartz(q =>
+        {
+            var priceJobKey = new JobKey("DailyPriceJob");
+
+            q.AddJob<GeneratePriceJob>(opts => opts.WithIdentity(priceJobKey));
+
+            q.AddTrigger(opts => opts
+                .ForJob(priceJobKey)
+                .WithIdentity("DailyPriceJob-trigger")
+                .WithCronSchedule("0 0 0 * * ?"));
+            //.WithSimpleSchedule(sched =>
+            //    sched.WithIntervalInMinutes(1)
+            //    .WithRepeatCount(1)));
+
+            var limitJobKey = new JobKey("PurchaseLimitJob");
+
+            q.AddJob<GeneratePurchaseLimitJob>(opts => opts.WithIdentity(limitJobKey));
+
+            q.AddTrigger(opts => opts
+                .ForJob(limitJobKey)
+                .WithIdentity("PurchaseLimitJob-trigger")
+                .WithCronSchedule("0 0 0 * * ?"));
+            //.WithSimpleSchedule(sched =>
+            //        sched.WithIntervalInMinutes(1)
+            //        .WithRepeatCount(1)));
+        });
+
+        services.AddQuartzHostedService(q =>
+        {
+            q.WaitForJobsToComplete = true;
+        });
 
         logger.LogInformation("{Project} services registered", "Infrastructure");
 
