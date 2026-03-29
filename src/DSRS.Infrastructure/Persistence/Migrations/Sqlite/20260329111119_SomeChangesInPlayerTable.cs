41ce475ccb49523a58d1b64096eb84f3260416b2
Kevz Macalalag <mc.kvnflrs@gmail.com>
2026-03-29 23:24:58 +0800

    Some changes related to ValueObjects in Entity Configuration Builder

---
+++ src/DSRS.Infrastructure/Persistence/Migrations/Sqlite/20260329111119_SomeChangesInPlayerTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DSRS.Infrastructure.Persistence.Migrations.Sqlite
+{
+    /// <inheritdoc />
+    public partial class SomeChangesInPlayerTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Balance",
+                table: "Players",
+                type: "TEXT",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Balance",
+                table: "Players",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT",
+                oldPrecision: 18,
+                oldScale: 2);
+        }
+    }
+}
